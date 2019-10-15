abd1519ffc5df2c831c501b0b6265670414d98c2
Dragos Micu <dragosmicu92@googlemail.com>
2019-10-15 12:00:23 +0100

    Update woocommerce-remove-add-to-cart-functionality.php

--- woocommerce-remove-add-to-cart-functionality.php
+++ woocommerce-remove-add-to-cart-functionality.php
@@ -1,5 +1,17 @@
 /** 
- * Hides Add to cart button and replaced it with Read more.
+ * Disables Add to cart button and replaced it with Read more.
  **/
 
 add_filter( 'woocommerce_is_purchasable', false );
+
+/**
+ * Disable all except specific products
+ */
+
+add_filter('woocommerce_is_purchasable', 'myplugin_is_purchasable', 10, 2);
+
+function myplugin_is_purchasable( $is_purchasable, $object ) {
+    // Checks to see if the product id is 22 or 23, 
+    // returns true if is, false otherwise.  
+    return ( 22 === $object->id || 23 === $object->id );
+}
